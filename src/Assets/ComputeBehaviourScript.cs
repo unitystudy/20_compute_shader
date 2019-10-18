6655892cfade5040e332d2f19c340b49df25ef9e
Takashi Imagire <imagire@gmail.com>
2019-10-18 11:13:27 +0900

    Add files via upload

--- src/Assets/ComputeBehaviourScript.cs
+++ src/Assets/ComputeBehaviourScript.cs
@@ -18,7 +18,7 @@ public class ComputeBehaviourScript : MonoBehaviour
     const int MAX_VERTEX_NUM = 65534;
 
     [SerializeField, Tooltip("This cannot be changed while running.")]
-    int maxParticleNum;
+    int maxInstanceNum = 10000;
     [SerializeField]
     Mesh mesh;
     [SerializeField]
@@ -27,18 +27,14 @@ public class ComputeBehaviourScript : MonoBehaviour
     ComputeShader computeShader;
 
     [SerializeField]
-    Vector3 velocity = new Vector3(2f, 5f, 2f);
-    [SerializeField]
-    Vector3 angVelocity = new Vector3(45f, 45f, 45f);
-    [SerializeField]
-    Vector3 range = new Vector3(100f, 0f, 100f);
+    Vector3 range = new Vector3(100f, 0f, 100f);// 広がる範囲
 
     Mesh combinedMesh_;
-    ComputeBuffer computeBuffer_;
+    ComputeBuffer computeBuffer_;// 計算シェーダ用バッファ
     int emitKernel_;
     List<Material> materials_ = new List<Material>();
-    int particleNumPerMesh_;
-    int meshNum_;
+    int numPerMesh_;// メッシュごとのインスタンス数
+    int meshNum_;// 描画メッシュ数
 
     Mesh CreateCombinedMesh(Mesh mesh, int num)
     {
@@ -90,25 +86,25 @@ public class ComputeBehaviourScript : MonoBehaviour
 
     void OnEnable()
     {
-        particleNumPerMesh_ = MAX_VERTEX_NUM / mesh.vertexCount;
-        meshNum_ = (int)Mathf.Ceil((float)maxParticleNum / particleNumPerMesh_);
+        numPerMesh_ = MAX_VERTEX_NUM / mesh.vertexCount;
+        meshNum_ = (int)Mathf.Ceil((float)maxInstanceNum / numPerMesh_);
 
         for (int i = 0; i < meshNum_; ++i)
         {
             var material = new Material(shader);
-            material.SetInt("_IdOffset", particleNumPerMesh_ * i);
+            material.SetInt("_IdOffset", numPerMesh_ * i);
             materials_.Add(material);
         }
 
-        combinedMesh_ = CreateCombinedMesh(mesh, particleNumPerMesh_);
-        computeBuffer_ = new ComputeBuffer(maxParticleNum, Marshal.SizeOf(typeof(Particle)), ComputeBufferType.Default);
+        combinedMesh_ = CreateCombinedMesh(mesh, numPerMesh_);
+        computeBuffer_ = new ComputeBuffer(maxInstanceNum, Marshal.SizeOf(typeof(Particle)), ComputeBufferType.Default);
 
         var initKernel = computeShader.FindKernel("Init");
         emitKernel_ = computeShader.FindKernel("Emit");
 
         computeShader.SetBuffer(initKernel, "_Particles", computeBuffer_);
         computeShader.SetVector("_Range", range);
-        computeShader.Dispatch(initKernel, maxParticleNum / 8, 1, 1);
+        computeShader.Dispatch(initKernel, maxInstanceNum / 8, 1, 1);
     }
 
     void OnDisable()
@@ -121,7 +117,7 @@ public class ComputeBehaviourScript : MonoBehaviour
         for (int i = 0; i < meshNum_; ++i)
         {
             var material = materials_[i];
-            material.SetInt("_IdOffset", particleNumPerMesh_ * i);
+            material.SetInt("_IdOffset", numPerMesh_ * i);
             material.SetBuffer("_Particles", computeBuffer_);
             Graphics.DrawMesh(combinedMesh_, transform.position, transform.rotation, material, 0);
         }
